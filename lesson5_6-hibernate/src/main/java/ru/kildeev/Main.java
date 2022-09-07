4449f53b280192be7bf2278e8fdd64d9fc2e0635
enverge <h4tesk@gmail.com>
2022-09-07 11:27:35 +0300

    lesson5 done

---
+++ lesson5_6-hibernate/src/main/java/ru/kildeev/Main.java
@@ -0,0 +1,62 @@
+package ru.kildeev;
+
+import org.hibernate.cfg.Configuration;
+import ru.kildeev.model.User;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+        EntityManagerFactory entityManagerFactory = new Configuration()
+                .configure("hibernate.cfg.xml")
+                .buildSessionFactory();
+
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+//        **SQL**INSERT**OPERATION**
+//        entityManager.getTransaction().begin();
+//
+//        entityManager.persist(new User("User1", "1@mail.ru", "pass1"));
+//        entityManager.persist(new User("User2", "2@mail.ru", "pass2"));
+//        entityManager.persist(new User("User3", "3@mail.ru", "pass3"));
+//
+//        entityManager.getTransaction().commit();
+
+        //Select
+//        User user = entityManager.find(User.class, 1L);
+//        System.out.println(user.toString());
+
+        //JPQL, HQL
+//        List<User> users = entityManager.createQuery("select u from User u", User.class)
+//                .getResultList();
+//        for (User userFromDb : users) {
+//            System.out.println(userFromDb);
+//        }
+
+
+        //UPDATE
+//        entityManager.getTransaction().begin();
+//        User user = entityManager.find(User.class, 1L);
+//        user.setUsername("new UserNAME");
+//        entityManager.getTransaction().commit();
+
+//       entityManager.getTransaction().begin();
+//       User user = new User ("User2New", "2@mail.ru", "pass2");
+//       user.setId(2L);
+//       entityManager.merge(user);
+//       entityManager.getTransaction().commit();
+
+        //DELETE
+//        entityManager.getTransaction().begin();
+
+//        entityManager.createQuery("delete from User u where u.id = 4").executeUpdate();
+//       User user = entityManager.find(User.class, 3L);
+//       entityManager.remove(user);
+//       entityManager.getTransaction().commit();
+
+        entityManager.close();
+        entityManagerFactory.close();
+    }
+}
---
+++ lesson5_6-hibernate/src/main/java/ru/kildeev/model/User.java
@@ -0,0 +1,36 @@
+package ru.kildeev.model;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
+@Table (name="users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false,length = 1024)
+    private String password;
+
+    public User(String username, String email, String password) {
+        this.username = username;
+        this.email = email;
+        this.password = password;
+    }
+}
